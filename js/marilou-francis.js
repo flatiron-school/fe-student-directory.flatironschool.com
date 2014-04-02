5970497505ad625cee5c516597cc6cfa6f7b156c
mgfrancis <opusmla@gmail.com>
2014-04-02 10:06:14 -0400

    add challenge10-jquery

---
+++ js/marilou-francis.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+/*$('img').click(function(){
+	$(this).css({'border-color','red'});
+});*/
+
+$('img').fadeIn(5000);
+$('img').fadeOut(5000);
+$('img').css({'border-color':'red'});
+$('img').fadeIn(5000);
+$('img').css({'border-style':'dotted'});
+$('img').css({'border-color':'lightblue'});
+
+
+});
